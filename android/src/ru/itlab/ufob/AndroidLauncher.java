2f8fffbfdfd7be1ac2b6f27cac5bee33ba482a25
Monster155 <damir180501@mail.ru>
2018-11-25 23:04:05 +0300

    #2

--- android/src/ru/itlab/ufob/AndroidLauncher.java
+++ android/src/ru/itlab/ufob/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import ru.itlab.game.Screens.MainActivity;
+import ru.itlab.ufob.Screens.MainActivity;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
