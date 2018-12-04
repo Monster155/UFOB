4e725b86ddd500ed07dd5c3b01a46af8b1a325fa
Damir <damir180501@mail.ru>
2018-12-04 22:20:23 +0300

    234

--- core/src/ru/itlab/ufob/Screens/SettingsScreen.java
+++ core/src/ru/itlab/ufob/Screens/SettingsScreen.java
@@ -2,11 +2,15 @@ package ru.itlab.ufob.Screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import ru.itlab.ufob.SpecialClasses.DialogWindow;
 
 public class SettingsScreen implements Screen {
 
@@ -15,6 +19,12 @@ public class SettingsScreen implements Screen {
     float width, height;
     SpriteBatch batch;
 
+    Stage stage;
+    Camera camera;
+
+    DialogWindow dw;
+    ResultsScreen rs;
+
     @Override
     public void show() {
         tB1 = new Texture("Settings/rename.png");//TODO
@@ -22,6 +32,12 @@ public class SettingsScreen implements Screen {
         width = Gdx.graphics.getWidth()/2f - tB1.getWidth() / tB1.getHeight()*height/2f;
         rB1 = new Rectangle(width, Gdx.graphics.getHeight()-height, tB1.getWidth() / tB1.getHeight()*height, height);
         batch = new SpriteBatch();
+
+        stage = new Stage();
+        camera = stage.getCamera();
+
+        rs = new ResultsScreen();
+        dw = new DialogWindow(rs);
     }
 
     @Override
@@ -36,14 +52,14 @@ public class SettingsScreen implements Screen {
                 rB1.height);
         batch.end();
 
-        /*if (Gdx.input.isTouched()) {
+        if (Gdx.input.isTouched()) {
             Vector3 touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
-            if (touchPos.x > rectBtn2.x && touchPos.x < rectBtn2.x + rectBtn2.width &&
-                    touchPos.y > rectBtn2.y && touchPos.y < rectBtn2.y + rectBtn2.height) {
-                screen = 2;
+            if (touchPos.x > rB1.x && touchPos.x < rB1.x + rB1.width &&
+                    touchPos.y > rB1.y && touchPos.y < rB1.y + rB1.height) {
+                Gdx.input.getTextInput(dw, "Enter your name for Records", "", "Enter Your Name");
             }
-        }*/
+        }
     }
 
     @Override
